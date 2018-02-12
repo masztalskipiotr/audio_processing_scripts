29a59cb1bc838197ca63d47b9e1891a14fde3063
Piotr Masztalski <masztalski.piotr@gmail.com>
2018-02-12 19:06:32 +0100

    Initial commit

---
+++ audio_scripts.py
@@ -0,0 +1,37 @@
+'''
+Author:    Piotr Masztalski
+Created:   12.02.2018
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io.wavfile import read as wavread
+
+
+def plot_wav(filename):
+    #reading the wave file
+    fs, signal = wavread(filename)
+
+    #creating a time vector
+    time = np.linspace(0, len(signal)/fs, num=len(signal))
+
+    #recalcucating the amplitude (range from -1 to 1)
+    if signal.dtype == 'int16':
+        bits = 16
+    elif signal.dtype == 'int32':
+        bits = 32
+    signal = signal/2**(bits - 1)
+
+    #plotting the signal
+    plt.figure(1)
+    plt.plot(time, signal)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time [s]")
+    plt.ylim(-1, 1)
+    plt.show()
+
+
+
+print(plot_wav('test_sound.wav'))
+
+
