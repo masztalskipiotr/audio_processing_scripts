d78fca9a132c006d12ab7020d5baf5aada2ae9f3
Piotr Masztalski <masztalski.piotr@gmail.com>
2018-02-12 21:34:57 +0100

    plot_fft function added

--- audio_scripts.py
+++ audio_scripts.py
@@ -1,31 +1,32 @@
-'''
+"""
 Author:    Piotr Masztalski
 Created:   12.02.2018
 
 A few useful functions for processing audio
-'''
+"""
 
-#required packages
+# required packages
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io.wavfile import read as wavread
+from soundfile import read
 
-#plot a signal
+
+# plot a signal
 def plot_wav(filename):
-    #reading the wave file
+    # reading the wave file
     fs, signal = wavread(filename)
 
-    #creating a time vector
+    # creating a time vector
     time = np.linspace(0, len(signal)/fs, num=len(signal))
 
-    #recalcucating the amplitude (range from -1 to 1)
+    # recalcucating the amplitude (range from -1 to 1)
     if signal.dtype == 'int16':
-        bits = 16
+        signal = signal / 2 ** 15
     elif signal.dtype == 'int32':
-        bits = 32
-    signal = signal/2**(bits - 1)
+        signal = signal / 2 ** 31
 
-    #plotting the signal
+    # plotting the signal
     plt.figure(1)
     plt.plot(time, signal)
     plt.ylabel("Amplitude")
@@ -33,6 +34,26 @@ def plot_wav(filename):
     plt.ylim(-1, 1)
     plt.show()
 
+
+# plot fft of a signal
+def plot_fft(filename):
+    # another option of reading the signal(no need to recalculate the amplitude)
+    signal, fs = read(filename)
+
+    # creating a frequency vector
+    freqs = np.arange(-fs/2, fs/2, step=fs/len(signal))
+
+    # computing and shifting the fft of the signal
+    my_fft = np.fft.fft(signal)
+    my_fft = np.fft.fftshift(my_fft)
+
+    # plotting the fft
+    plt.figure(1)
+    plt.plot(freqs, abs(my_fft))
+    plt.ylabel("Amplitude")
+    plt.xlabel("Frequency [Hz]")
+    plt.xlim(0, 20000)  # adjusting the frequency axis to the human hearing range
+    plt.show()
 
 
 
